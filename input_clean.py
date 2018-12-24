1eb18228cb87624121650874176f107637f23e7c
David Yang <saluteyang@gmail.com>
2018-12-24 13:54:05 -0500

    Initial commit.

---
+++ input_clean.py
@@ -0,0 +1,42 @@
+import csv
+from datetime import datetime, timedelta
+from matplotlib import pyplot as plt
+
+filename = 'Houston_tx_hobby_2010-2017.csv'
+
+# choose only REPORTTPYE (misspelt) FM-15
+# exclude time periods when hourly drybulb or humidity data are missing
+# missing data can be blanks or wildcard character *
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    # print header with position
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
+
+    dates, drybulb, humidity = [], [], []
+    for row in reader:
+        if row[6] == 'FM-15':
+            try:
+                date_obs = datetime.strptime(row[5], "%Y-%m-%d %H:%M")
+                drybulb_temp = int(row[10])
+                relative_humidity = int(row[16])
+
+            except ValueError:
+                print(date_obs, 'missing data')
+
+            else:
+                dates.append(date_obs)
+                drybulb.append(drybulb_temp)
+                humidity.append(relative_humidity)
+
+# create datetime series
+
+dt = datetime(2010, 1, 1, 0)
+end_dt = datetime(2017, 12, 31, 23)
+step = timedelta(hours=1)
+time_seq = []
+while dt <= end_dt:
+    time_seq.append(dt)
+    dt += step
