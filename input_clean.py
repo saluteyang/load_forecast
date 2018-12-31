62cb93772231994ad30552beeacab563e425b794
David Yang <saluteyang@gmail.com>
2018-12-31 15:53:05 -0500

    Trained a few different configurations.

--- input_clean.py
+++ input_clean.py
@@ -281,41 +281,162 @@ test_steps = (len(joined_keep) - 60001 - lookback)
 
 def build_model2():
     model = models.Sequential()
-    model.add(layers.GRU(20, input_shape=(None, joined_keep.shape[-1])))
+    model.add(layers.GRU(32, return_sequences=True, input_shape=(None, joined_keep.shape[-1])))
+    model.add(layers.GRU(64, activation='relu'))
     model.add(layers.Dense(1))
     model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
     return model
 
 model2 = build_model2()
 history = model2.fit_generator(train_gen,
-                              steps_per_epoch=50,
-                              epochs=5,
+                              steps_per_epoch=350,
+                              epochs=20,
                               validation_data=val_gen,
-                               validation_steps=50)
+                              validation_steps=70)
 
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 history_dict = history.history
 epochs = range(1, len(history_dict['loss']) + 1)
 
-plt.plot(epochs, loss, 'bo', label='training loss')  # blue dot
-plt.plot(epochs, val_loss, 'b', label='validation loss')
+plt.plot(epochs[2:], loss[2:], 'bo', label='training loss')  # blue dot
+plt.plot(epochs[2:], val_loss[2:], 'b', label='validation loss')
 plt.title('training and validation loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
 plt.show()
 
-# to do
-# 1 compare to seasonal arima
-# 2 compare to times series prediction tools
-# 3 validate layers and epochs
-# 4 provide list of holidays
-# 5 compare with other neural network configurations
-
-predictions = model2.predict_generator(test_gen, steps=50)
+predictions = model2.predict_generator(test_gen, steps=70)
 plt.plot(range(1000), predictions[:1000], 'r', label='test predictions', alpha=0.2)
+plt.plot(range(1000), joined_keep[:1000, 0], 'b', label='test actual', alpha=0.2)
 plt.xticks(rotation=90)
 plt.legend()
 plt.show()
 
+model2.evaluate_generator(test_gen, steps=70)
+
+
+# experiments to improve accuracy:
+# have attempted different number of hidden units, including second GRU layer,
+# using LSTM instead of GRU and increasing epochs
+
+def build_model3():
+    model = models.Sequential()
+    model.add(layers.LSTM(64, input_shape=(None, joined_keep.shape[-1])))
+    model.add(layers.Dense(1))
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    return model
+
+model3 = build_model3()
+history = model3.fit_generator(train_gen,
+                              steps_per_epoch=350,
+                              epochs=15,
+                              validation_data=val_gen,
+                              validation_steps=70)
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+history_dict = history.history
+epochs = range(1, len(history_dict['loss']) + 1)
+
+plt.plot(epochs[2:], loss[2:], 'bo', label='training loss')  # blue dot
+plt.plot(epochs[2:], val_loss[2:], 'b', label='validation loss')
+plt.title('training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+predictions = model3.predict_generator(test_gen, steps=70)
+plt.plot(range(1000), predictions[:1000], 'r', label='test predictions', alpha=0.2)
+plt.plot(range(1000), joined_keep[:1000, 0], 'b', label='test actual', alpha=0.2)
+plt.xticks(rotation=90)
+plt.legend()
+plt.show()
+
+model3.evaluate_generator(test_gen, steps=70)
+
+# another attempt is to include the calendar dummies in the input data
+
+joined_keep['Hour_Num'] = joined_keep.index.hour
+joined_keep['Day_Num'] = joined_keep.index.weekday  # Monday is 0
+joined_keep['Wknd_Flag'] = (joined_keep.index.weekday > 4) * 1
+
+joined_keep['Date'] = joined_keep.index.date
+
+import holidays
+
+us_holidays = holidays.UnitedStates()
+joined_keep['Holiday_Flag'] = [(x in us_holidays) * 1 for x in joined_keep['Date']]
+joined_keep['Off_Flag'] = joined_keep[['Wknd_Flag', 'Holiday_Flag']].max(axis=1)
+joined_keep = joined_keep.dropna()
+joined_keep = joined_keep.drop(columns=['Date', 'Wknd_Flag', 'Holiday_Flag'])
+joined_keep_float = joined_keep.drop(columns=['Hour_Num', 'Day_Num', 'Off_Flag', 'Drybulb', 'Humidity'])
+joined_keep_int = joined_keep[['Hour_Num', 'Day_Num', 'Off_Flag', 'Drybulb', 'Humidity']]
+joined_keep_float = joined_keep_float/1000
+joined_keep_int = joined_keep_int.astype('int')
+joined_keep = joined_keep_float.merge(joined_keep_int, left_index=True, right_index=True)
+joined_keep = joined_keep.values
+
+lookback = 336
+delay = 24
+batch_size = 168
+
+train_gen = generator(joined_keep,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=0,
+                      max_index=50000,
+                      batch_size=batch_size)
+
+val_gen = generator(joined_keep,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=50001,
+                      max_index=60000,
+                      batch_size=batch_size)
+
+test_gen = generator(joined_keep,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=60001,
+                      max_index=None,
+                      batch_size=batch_size)
+
+def build_model4():
+    model = models.Sequential()
+    model.add(layers.GRU(32, return_sequences=True, input_shape=(None, joined_keep.shape[-1])))
+    model.add(layers.GRU(64, activation='relu'))
+    model.add(layers.Dense(1))
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    return model
+
+model4 = build_model4()
+history = model4.fit_generator(train_gen,
+                              steps_per_epoch=290,
+                              epochs=20,
+                              validation_data=val_gen,
+                              validation_steps=55)
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+history_dict = history.history
+epochs = range(1, len(history_dict['loss']) + 1)
+
+plt.plot(epochs[2:], loss[2:], 'bo', label='training loss')  # blue dot
+plt.plot(epochs[2:], val_loss[2:], 'b', label='validation loss')
+plt.title('training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+predictions = model4.predict_generator(test_gen, steps=55)
+plt.plot(range(1000), predictions[:1000], 'r', label='test predictions', alpha=0.2)
+plt.plot(range(1000), joined_keep[:1000, 0], 'b', label='test actual', alpha=0.2)
+plt.xticks(rotation=90)
+plt.legend()
+plt.show()
+
+model4.evaluate_generator(test_gen, steps=55)
