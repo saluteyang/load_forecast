ab5ed7cfd7be683d74d7fea8ae0d8c74d4d2671a
David Yang <saluteyang@gmail.com>
2019-01-05 17:19:42 -0500

    Time series plots, decision tree, layer order

--- input_clean.py
+++ input_clean.py
@@ -9,6 +9,8 @@ import holidays
 from keras import models
 from keras import layers
 import os
+import statsmodels.tsa.api as smt
+import seaborn as sns
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
@@ -50,8 +52,8 @@ weather_hr = weather.groupby([lambda x: x.date, lambda x: x.hour])['Drybulb', 'H
 # plot using index needs to turn index into a column first
 # note that multi-index will be turned into columns by level
 weather_hr['Hour_End'] = pd.to_datetime(weather_hr.index.map(lambda x: '-'.join((str(x[0]), str(x[1])))), format='%Y-%m-%d-%H')
-weather_hr.plot(x='Hour_End', y='Drybulb')
-plt.show()
+# weather_hr.plot(x='Hour_End', y='Drybulb')
+# plt.show()
 
 # weather['Date'] = weather['Hour_End'].dt.date
 # weather['Hour'] = weather['Hour_End'].dt.hour
@@ -90,15 +92,46 @@ aggregate_load = pd.DataFrame()
 # start here to import processed data, if using other profiles, run section in profile_proc.py ###############
 with open('test.csv', 'r') as f:
     aggregate_load = pd.read_csv(f, index_col=0)
-aggregate_load['COAST'] = aggregate_load['COAST']/1000
+aggregate_load['COAST'] = aggregate_load['COAST']/1000  # source data units are in MW, here converted to GW
 
 # plot time series with matplotlib
-aggregate_load.plot(x='Hour_End', y='ERCOT')
-plt.show()
+# aggregate_load.plot(x='Hour_End', y='ERCOT')
+# plt.show()
 
 # join weather and load information
 aggregate_load = aggregate_load.set_index('Hour_End')
 aggregate_load.index = pd.to_datetime(aggregate_load.index)
+
+# optional plots for time series EDA
+
+plt.figure()
+aggregate_load['COAST'].plot()
+plt.xlabel('Year')
+plt.show()
+
+subsample = aggregate_load['COAST'][:336]
+
+plt.figure()
+layout = (2, 2)
+ts_ax = plt.subplot2grid(layout, (0, 0))
+hist_ax = plt.subplot2grid(layout, (0, 1))
+acf_ax = plt.subplot2grid(layout, (1, 0))
+pacf_ax = plt.subplot2grid(layout, (1, 1))
+
+subsample.plot(ax=ts_ax)
+ts_ax.set_title('')
+ts_ax.set_xlabel('')
+# ts_ax.xaxis.set_major_formatter(mdates.DateFormatter('%d'))  # did not work; import matplotlib.dates as mdates
+# for tick in ts_ax.get_xticklabels():
+#     tick.set_rotation(90)
+subsample.plot(ax=hist_ax, kind='hist', bins=25)
+hist_ax.set_title('Histogram')
+smt.graphics.plot_acf(subsample, lags=48, ax=acf_ax)
+smt.graphics.plot_pacf(subsample, lags=48, ax=pacf_ax)
+[ax.set_xlim(0) for ax in [acf_ax, pacf_ax]]
+sns.despine()
+plt.tight_layout()
+plt.show()
 
 # section end, joining weather and load data begins below ##############################################
 
@@ -107,9 +140,9 @@ joined = aggregate_load.join(weather_hr, how='inner')
 
 joined[['COAST', 'Drybulb', 'Humidity']].corr()
 
-lm = sm.ols(formula='COAST ~ Drybulb + Humidity', data=joined).fit()
-lm.params
-lm.summary()
+# lm = sm.ols(formula='COAST ~ Drybulb + Humidity', data=joined).fit()
+# lm.params
+# lm.summary()
 
 # the relationship is not linear
 joined.plot(x='Drybulb', y='COAST', kind='scatter')
@@ -168,6 +201,29 @@ joined_clean_test = joined_clean[joined_clean.index.year==2017]
 test_data = joined_clean_test.drop(columns=['COAST_Hourly'])
 test_target = joined_clean_test['COAST_Hourly']
 
+# decision tree model (baseline?)
+
+from sklearn.tree import DecisionTreeRegressor
+
+model0 = DecisionTreeRegressor(min_samples_leaf=20).fit(train_data, train_target)
+
+print('training accuracy: {:.3f}'.format(model0.score(train_data, train_target)))  # here score returns R-square
+print('test accuracy: {:.3f}'.format(model0.score(test_data, test_target)))
+
+predictions = model0.predict(test_data)
+plt.plot(test_target.index[:1000], test_target[:1000], 'b', label='test actual', alpha=0.2)
+plt.plot(test_target.index[:1000], predictions[:1000], 'r', label='test predictions', alpha=0.2)
+plt.xticks(rotation=90)
+plt.legend()
+plt.show()
+
+# training accuracy: 0.960
+# test accuracy: 0.903
+
+# using the same accuracy definition as for model1 below
+mae_pct = 1 - np.mean(abs(predictions - test_target.values.reshape(len(test_target.values), 1)))/np.mean(test_target.values)
+# test accuracy: 0.95555
+
 # model1: using regression model
 
 def build_model():
@@ -176,7 +232,7 @@ def build_model():
                            input_shape=(train_data.shape[1],)))
     model.add(layers.Dense(20, activation='relu'))
     model.add(layers.Dense(1))
-    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])  # in Keras, accuracy is a classification metric
     return model
 
 model = build_model()
@@ -210,6 +266,11 @@ plt.legend()
 plt.show()
 
 model.evaluate(test_data, test_target)
+test_mae = model.evaluate(test_data, test_target)[1]
+# using the following accuracy definition
+print('test accuracy: {:.5f}'.format(1-test_mae/test_target.mean()))
+
+# test accuracy: 0.95497
 
 # results: mae on training 0.4993, on validation 0.6769, on test 0.5430
 # results: mae on training 0.2608, on validation 0.3438, on test 0.3932 (busmed)
@@ -504,7 +565,7 @@ def build_model5():
 model5 = build_model5()
 history = model5.fit_generator(train_gen,
                               steps_per_epoch=290,
-                              epochs=40,
+                              epochs=20,
                               validation_data=val_gen,
                               validation_steps=55)
 
@@ -532,3 +593,83 @@ model5.evaluate_generator(test_gen, steps=55)
 
 # results: mae on training 0.7444, on validation 1.1637, on test 1.3112 (20 epochs)
 # results: mae on training 0.6535, on validation 1.5005, on test 1.6972 (40 epochs)
+
+# model6: add a dense layer *after* the GRU layer, keeping dummies
+
+joined_keep['Hour_Num'] = joined_keep.index.hour
+joined_keep['Day_Num'] = joined_keep.index.weekday  # Monday is 0
+joined_keep['Wknd_Flag'] = (joined_keep.index.weekday > 4) * 1
+joined_keep['Date'] = joined_keep.index.date
+
+us_holidays = holidays.UnitedStates()
+
+joined_keep['Holiday_Flag'] = [(x in us_holidays) * 1 for x in joined_keep['Date']]
+joined_keep['Off_Flag'] = joined_keep[['Wknd_Flag', 'Holiday_Flag']].max(axis=1)
+joined_keep = joined_keep.dropna()
+joined_keep = joined_keep.drop(columns=['Date', 'Wknd_Flag', 'Holiday_Flag'])
+joined_keep_float = joined_keep.drop(columns=['Hour_Num', 'Day_Num', 'Off_Flag', 'Drybulb', 'Humidity'])
+joined_keep_int = joined_keep[['Hour_Num', 'Day_Num', 'Off_Flag', 'Drybulb', 'Humidity']]
+joined_keep_int = joined_keep_int.astype('int')
+joined_keep = joined_keep_float.merge(joined_keep_int, left_index=True, right_index=True)
+joined_keep = joined_keep.values
+
+train_gen = generator(joined_keep,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=0,
+                      max_index=50000,
+                      batch_size=batch_size)
+
+val_gen = generator(joined_keep,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=50001,
+                      max_index=60000,
+                      batch_size=batch_size)
+
+test_gen = generator(joined_keep,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=60001,
+                      max_index=None,
+                      batch_size=batch_size)
+
+def build_model6():
+    model = models.Sequential()
+    model.add(layers.GRU(64,  # return_sequences=True,
+                         input_shape=(None, joined_keep.shape[-1])))
+    model.add(layers.Dense(20, activation='relu'))
+    model.add(layers.Dense(1))
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    return model
+
+model6 = build_model6()
+history = model6.fit_generator(train_gen,
+                              steps_per_epoch=290,
+                              epochs=20,
+                              validation_data=val_gen,
+                              validation_steps=55)
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+history_dict = history.history
+epochs = range(1, len(history_dict['loss']) + 1)
+
+plt.plot(epochs[2:], loss[2:], 'bo', label='training loss')  # blue dot
+plt.plot(epochs[2:], val_loss[2:], 'b', label='validation loss')
+plt.title('training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+predictions = model6.predict_generator(test_gen, steps=55)
+plt.plot(range(1000), predictions[:1000], 'r', label='test predictions', alpha=0.2)
+plt.plot(range(1000), joined_keep[:1000, 0], 'b', label='test actual', alpha=0.2)
+plt.xticks(rotation=90)
+plt.legend()
+plt.show()
+
+model6.evaluate_generator(test_gen, steps=55)
+
+# results: mae on training 0.684, on validation 1.706, on test 1.983 (20 epochs)
