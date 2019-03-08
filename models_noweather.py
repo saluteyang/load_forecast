898443eec6f6f87376da7087ad10dc1b0fae4fb7
David Yang <saluteyang@gmail.com>
2019-03-07 20:51:10 -0500

    Rewrote code and re-orged code files

---
+++ models_noweather.py
@@ -0,0 +1,135 @@
+import csv
+import datetime as dt
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import holidays
+from keras import models
+from keras import layers
+from keras import callbacks
+# import keras
+import os
+import statsmodels.tsa.api as smt
+import seaborn as sns
+import matplotlib.gridspec as gs
+from sklearn.tree import DecisionTreeRegressor
+from sklearn import preprocessing
+import numpy as np
+
+from models_noweather_helpers import *
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+# data processing step #############################
+# import processed data; if using other profiles, run section in profile_proc.py and continue below ###############
+aggregate_load = pd.DataFrame()
+with open('test.csv', 'r') as f:
+    aggregate_load = pd.read_csv(f, index_col=0)
+
+# source data units are in MW, here converted to GW
+aggregate_load = aggregate_load.set_index('Hour_End')
+aggregate_load = aggregate_load.apply(lambda x: x/1000)
+aggregate_load.index = pd.to_datetime(aggregate_load.index)
+
+# duplicate index due to additional hour in Nov due to DST
+joined = aggregate_load.groupby(aggregate_load.index).first()  
+joined = joined.dropna().copy()
+
+# create indicator variables
+joined['Hour_Num'] = joined.index.hour
+joined['Day_Num'] = joined.index.weekday  # Monday is 0
+joined['Wknd_Flag'] = (joined.index.weekday > 4) * 1
+joined['Date'] = joined.index.date
+
+# add holidays flag
+us_holidays = holidays.UnitedStates()  # this creates a dictionary
+joined['Holiday_Flag'] = [(x in us_holidays) * 1 for x in joined['Date']]
+joined['Off_Flag'] = joined[['Wknd_Flag', 'Holiday_Flag']].max(axis=1)
+joined = joined.drop(columns=['Date', 'Wknd_Flag', 'Holiday_Flag'])
+
+# create training and testing data sets, generator will separate out the features and target
+train_data = joined[joined.index.year != 2017]
+test_data = joined[joined.index.year == 2017]
+
+# added section for standardization
+scaler = preprocessing.MinMaxScaler()
+train_data = scaler.fit_transform(train_data)
+test_data = scaler.transform(test_data)
+
+# create generators using udf in helpers file
+lookback = 1440  # 60 days
+delay = 24  # how far into the future is the target
+
+train_gen = generator(train_data,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=0,
+                      max_index=50000)
+val_gen = generator(train_data,
+                    lookback=lookback,
+                    delay=delay,
+                    min_index=50001,
+                    max_index=None)
+test_gen = generator(test_data,
+                     lookback=lookback,
+                     delay=delay,
+                     min_index=0,
+                     max_index=None)
+
+# val_steps = (60000 - 50001 - lookback)
+# test_steps = (len(joined) - 60001 - lookback)
+
+# regression model  #########################################################################################
+
+def build_model():
+    model = models.Sequential()
+    model.add(layers.Flatten(input_shape=(lookback, joined.shape[-1])))
+    model.add(layers.Dense(20, activation='relu'))
+    model.add(layers.Dense(20, activation='relu'))
+    model.add(layers.Dense(1))
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    return model
+
+model = build_model()
+history = model.fit_generator(train_gen,
+                              epochs=20,
+                              steps_per_epoch=100,
+                              validation_data=val_gen,
+                              validation_steps=100)
+
+test_metrics = model.evaluate_generator(test_gen, steps=24)
+test_mae = test_metrics[1]
+# using the following accuracy definition
+print('test accuracy: {:.5f}'.format(1-test_mae/test_data[:24, 0].mean()))
+# test accuracy: 0.45951
+
+loss_pred_plots(history=history, skip_epoch=0, model=model, \
+                test=test_gen, test_target=test_data[:,0], pred_periods=48)
+
+# the RNN models ####################################################################
+
+# early stopping?
+def build_rnn():
+    model = models.Sequential()
+    model.add(layers.GRU(64, input_shape=(None, train_data.shape[-1])))
+    model.add(layers.Dense(1))
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    return model
+
+model_rnn = build_rnn()
+history_rnn = model_rnn.fit_generator(train_gen,
+                                      steps_per_epoch=100,
+                                      epochs=20,
+                                      validation_data=val_gen,
+                                      validation_steps=100
+                                      # callbacks=[
+                                      #     callbacks.EarlyStopping(patience=5, verbose=1,
+                                      #                             restore_best_weights=True)
+                                      # ]
+                                      )
+
+
+test_metrics_rnn = model.evaluate_generator(test_gen, steps=24)
+test_mae = test_metrics_rnn[1]
+# using the following accuracy definition
+print('test accuracy: {:.5f}'.format(1-test_mae/test_data[:24, 0].mean()))
---
+++ models_noweather_helpers.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def loss_pred_plots(history, skip_epoch, model, test, test_target, pred_periods):
+    # prepare the data for train vs validation loss
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    history_dict = history.history
+    epochs = range(1, len(history_dict['loss']) + 1)
+
+    # get the data for predictions based on model
+    predictions = model.predict_generator(test, steps=48)
+
+#     skip_epoch = 2
+#     pred_periods = 500
+#     test_target = joined['COAST'][60001:]
+
+    f, axarr = plt.subplots(2, sharex=False)
+
+    axarr[0].plot(epochs[skip_epoch:], loss[skip_epoch:], 'r', label='training loss')
+    axarr[0].plot(epochs[skip_epoch:], val_loss[skip_epoch:], 'b', label='validation loss')
+    axarr[0].set_title('training and validation loss')
+    axarr[0].set_xlabel('Epochs')
+    axarr[0].set_ylabel('Loss')
+    axarr[0].legend()
+
+    axarr[1].plot(range(pred_periods), predictions[:pred_periods], 'r', label='test predictions', alpha=0.2)
+    axarr[1].plot(range(pred_periods), test_target[:pred_periods], 'b', label='test actual', alpha=0.2)
+    axarr[1].set_title(f'first {pred_periods} periods predictions vs actual')
+    axarr[1].tick_params(axis='x', rotation=70)
+    # axarr[1].xticks(rotation=90)
+    axarr[1].legend()
+
+    f.subplots_adjust(hspace=0.7)
+    plt.show()
+
+# new generator function for samples and targets
+# move the target variable (regional load to predict) to the first column position
+def generator(data, lookback, delay, min_index, max_index,
+              shuffle=False, batchsize=168, step=1):
+    if max_index is None:
+        max_index = len(data) - delay - 1
+    i = min_index + lookback
+    while 1:
+        if shuffle:
+            rows = np.random.randint(min_index + lookback, max_index, size=batchsize)
+        else:
+            if i + batchsize >= max_index:
+                i = min_index + lookback
+            # np.arange(start, stop(not including))
+            rows = np.arange(i, min(i + batchsize, max_index))
+            i += len(rows)
+        samples = np.zeros((len(rows),
+                           lookback//step,
+                           data.shape[-1]))
+        targets = np.zeros(len(rows), )
+        for j, row in enumerate(rows):
+            indices = range(rows[j] - lookback, rows[j], step)
+            samples[j] = data[indices]
+            targets[j] = data[rows[j] + delay][0]
+        yield samples, targets
